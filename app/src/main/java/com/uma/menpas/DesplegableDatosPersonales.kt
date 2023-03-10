dc8d60f0c1e605e8a44854dab2cc6cfb5275fafd
franbono2 <franbono2@gmail.com>
2023-03-10 19:17:36 +0100

    desplegable_dp con botones toast funcionando

--- app/src/main/java/com/uma/menpas/DesplegableDatosPersonales.kt
+++ app/src/main/java/com/uma/menpas/DesplegableDatosPersonales.kt
@@ -3,6 +3,8 @@ package com.uma.menpas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RelativeLayout
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class DesplegableDatosPersonales : AppCompatActivity() {
@@ -17,8 +19,32 @@ class DesplegableDatosPersonales : AppCompatActivity() {
             val dialog = BottomSheetDialog(this)
             val view = layoutInflater.inflate(R.layout.desplegable_datos_personales, null)
 
+            val btnModDatos = view.findViewById<RelativeLayout>(R.id.RLModDatos)
+            btnModDatos.setOnClickListener {
+                showToast("Activity: Modificar Datos")
+            }
+            val btnConEstadisticas = view.findViewById<RelativeLayout>(R.id.RLConEstadisticas)
+            btnConEstadisticas.setOnClickListener {
+                showToast("Activity: Consultar Estadisticas")
+            }
+            val btnRegCentro = view.findViewById<RelativeLayout>(R.id.RLRegCentro)
+            btnRegCentro.setOnClickListener {
+                showToast("Activity: Registrar Centro")
+            }
+            val btnCambiarPerfil = view.findViewById<RelativeLayout>(R.id.RLCambioPerfil)
+            btnCambiarPerfil.setOnClickListener {
+                showToast("Activity: Cambiar Perfil")
+            }
+            val btnDarBaja = view.findViewById<RelativeLayout>(R.id.RLDarBaja)
+            btnDarBaja.setOnClickListener {
+                showToast("Activity: Dar de Baja")
+            }
+
             dialog.setContentView(view)
             dialog.show()
         }
+    }
+    fun showToast(msg: String){
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
 }
