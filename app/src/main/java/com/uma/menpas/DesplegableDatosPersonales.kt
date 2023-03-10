f2374fce99d919b992dc473eaff5554d2f098d16
franbono2 <franbono2@gmail.com>
2023-03-10 13:09:26 +0100

    añadido boton para accionar el desplegable

--- app/src/main/java/com/uma/menpas/DesplegableDatosPersonales.kt
+++ app/src/main/java/com/uma/menpas/DesplegableDatosPersonales.kt
@@ -2,10 +2,23 @@ package com.uma.menpas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class DesplegableDatosPersonales : AppCompatActivity() {
+
+    lateinit var btnMostrarDesplegable: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_desplegable_datos_personales)
+
+        btnMostrarDesplegable = findViewById(R.id.idBtnShowBottomSheet)
+        btnMostrarDesplegable.setOnClickListener {
+            val dialog = BottomSheetDialog(this)
+            val view = layoutInflater.inflate(R.layout.desplegable_datos_personales, null)
+
+            dialog.setContentView(view)
+            dialog.show()
+        }
     }
 }
