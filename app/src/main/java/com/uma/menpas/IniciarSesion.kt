139d26004105aa39c0f76bbb3597e696cfe88002
MiguelAngelCabanillas <0619851753@uma.es>
2023-03-11 23:38:01 +0100

    Primeras figuras de iniciar sesion

---
+++ app/src/main/java/com/uma/menpas/IniciarSesion.kt
@@ -0,0 +1,11 @@
+package com.uma.menpas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IniciarSesion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iniciar_sesion)
+    }
+}
