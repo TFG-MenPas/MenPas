db71609167952899603d2659da6318b93eb542d9
danij <danijlg92@uma.es>
2023-03-12 18:46:15 +0100

    Ajustados items menu inferior, botones y menu preparados para implementacion

--- app/src/main/java/com/uma/menpas/DatosPersonales.kt
+++ app/src/main/java/com/uma/menpas/DatosPersonales.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.CheckBox
 import android.widget.ImageButton
 import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DatosPersonales : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,7 @@ class DatosPersonales : AppCompatActivity() {
         val botonEditarNombre = findViewById<ImageButton>(R.id.botonEditarNombre)
         val botonEditarApellidos = findViewById<ImageButton>(R.id.botonEditarApellidos)
         val checkBoxSuscripcion = findViewById<CheckBox>(R.id.checkBoxSuscripcion)
+        val barraNavegacionInferior = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
         botonEditarNombreDeUsuario.setOnClickListener{
             Toast.makeText(applicationContext, "Edita el nombre de usuario", Toast.LENGTH_SHORT)
@@ -40,6 +42,29 @@ class DatosPersonales : AppCompatActivity() {
                     .show()
             }
 
+        }
+
+        barraNavegacionInferior.getMenu().getItem(0).setCheckable(false); // LINEA PROVISIONAL PARA QUITAR EL FOCUS INICIAL DEL PRIMER ELEMENTO
+        barraNavegacionInferior.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_faq -> {
+                    it.setCheckable(true); // LINEA PROVISIONAL PARA DEVOLVER EL CHECKABLE AL PRIMER ELEMENTO
+                    Toast.makeText(applicationContext, "FAQ", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_home -> {
+                        Toast.makeText(applicationContext, "HOME", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_user -> {
+                    Toast.makeText(applicationContext, "USER", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
         }
     }
 }
