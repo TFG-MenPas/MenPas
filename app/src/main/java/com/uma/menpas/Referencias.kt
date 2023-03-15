bc389b421b14dc519df2fa62750c6101d29d70fb
MiguelAngelCabanillas <0619851753@uma.es>
2023-03-15 21:32:04 +0100

    Activity de referencias terminado con links funcionales

--- app/src/main/java/com/uma/menpas/Referencias.kt
+++ app/src/main/java/com/uma/menpas/Referencias.kt
@@ -2,10 +2,18 @@ package com.uma.menpas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 
 class Referencias : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_referencias)
+
+        val textLinkReferencia1 = findViewById<TextView>(R.id.textLinkReferencia1)
+        val textLinkReferencia2 = findViewById<TextView>(R.id.textLinkReferencia2)
+
+        textLinkReferencia1.movementMethod = LinkMovementMethod.getInstance();
+        textLinkReferencia2.movementMethod = LinkMovementMethod.getInstance();
     }
 }
