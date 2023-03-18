fc3a2513b675358ba3a34331b10ba00a237a669b
manuel-agm <manuel.agm@uma.es>
2023-03-18 15:26:41 +0100

    Refactorización de Area1

--- app/src/main/java/com/uma/menpas/Area1.kt
+++ app/src/main/java/com/uma/menpas/Area1.kt
@@ -1,31 +1,33 @@
 package com.uma.menpas
-import org.json.JSONObject
-import android.content.res.Resources
-import android.graphics.Color
-import android.graphics.Typeface
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.renderscript.ScriptGroup.Input
 import android.view.Gravity
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import org.json.JSONArray
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
-import java.io.InputStream
 import java.io.InputStreamReader
 
 class Area1 : AppCompatActivity() {
+
+    companion object {
+        private const val JSON_RESOURCE_NAME = "areas"
+        private const val JSON_RESOURCE_TYPE = "raw"
+        private const val BUTTON_TEXT_SIZE = 18f
+        private const val AREA = "Ansiedad"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_area1)
 
-        //AQUÍ DEBERÍAMOS PASARLE EL INTENT, NO EL NOMBRE HARDCODEADO
-        val area = "Calidad de vida"
         val linearLayout = findViewById<LinearLayout>(R.id.botones_areas)
-        this.drawTitle(area)
-        this.iterateSections(this.getSections(area), linearLayout)
+        drawTitle(AREA)
+        iterateSections(getSections(AREA), linearLayout)
     }
 
     private fun drawTitle(area: String) {
@@ -33,29 +35,30 @@ class Area1 : AppCompatActivity() {
         textArea.text = area
     }
 
-    private fun getSections(area: String): ArrayList<String> {
-        val jsonString = this.transformJSONtoString()
+    private fun getSections(area: String): List<String> {
+        val jsonString = transformJSONtoString()
         val jsonObject = JSONObject(jsonString)
-        return (jsonObject.toDict().get(area)) as ArrayList<String>
+        return jsonObject.toDict()[area] as List<String>
     }
 
-    private fun iterateSections(sections: ArrayList<String>, linearLayout: LinearLayout) {
+    private fun iterateSections(sections: List<String>, linearLayout: LinearLayout) {
         for (section in sections) {
-            val boton = Button(this)
-            boton.text = section.toString()
-            boton.textSize = 18f
-            boton.isAllCaps = false
-            boton.gravity = Gravity.CENTER
-            boton.setTextColor(getResources().getColor(R.color.dark_blue))
-            boton.setTypeface(resources.getFont(R.font.poppins_bold))
-            boton.setBackgroundResource((R.drawable.button_white))
+            val boton = Button(this).apply {
+                text = section
+                textSize = BUTTON_TEXT_SIZE
+                isAllCaps = false
+                gravity = Gravity.CENTER
+                setTextColor(resources.getColor(R.color.dark_blue))
+                setTypeface(resources.getFont(R.font.poppins_bold))
+                setBackgroundResource(R.drawable.button_white)
+            }
             linearLayout.addView(boton)
         }
     }
 
     private fun transformJSONtoString(): String {
         val res = resources
-        val resId = res.getIdentifier("areas", "raw", packageName)
+        val resId = res.getIdentifier(JSON_RESOURCE_NAME, JSON_RESOURCE_TYPE, packageName)
 
         val inputStream = res.openRawResource(resId)
         val reader = BufferedReader(InputStreamReader(inputStream))
@@ -74,12 +77,12 @@ class Area1 : AppCompatActivity() {
         return sb.toString()
     }
 
-    fun JSONObject.toDict(): Map<String, *> = keys().asSequence().associateWith {
+    private fun JSONObject.toDict(): Map<String, *> = keys().asSequence().associateWith {
         when (val value = this[it])
         {
             is JSONArray ->
             {
-                val map = (0 until value.length()).associate { Pair(it.toString(), value[it]) }
+                val map = (0 until value.length()).associate { index -> index.toString() to value[index] }
                 JSONObject(map).toDict().values.toList()
             }
             is JSONObject -> value.toDict()
