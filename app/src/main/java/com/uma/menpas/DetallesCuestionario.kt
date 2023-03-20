53dd47be6ddb0cde83d0fe846412ec8c7919c06f
MiguelAngelCabanillas <0619851753@uma.es>
2023-03-20 13:21:14 +0100

    Detalles de cuestionario creado

---
+++ app/src/main/java/com/uma/menpas/DetallesCuestionario.kt
@@ -0,0 +1,11 @@
+package com.uma.menpas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetallesCuestionario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalles_cuestionario)
+    }
+}
