f7f53bb4c29b14c84801a08f0be8887221f20e08
MiguelAngelCabanillas <0619851753@uma.es>
2023-03-20 14:39:31 +0100

    Focus de barra quitado

--- app/src/main/java/com/uma/menpas/DudasSugerencias.kt
+++ app/src/main/java/com/uma/menpas/DudasSugerencias.kt
@@ -2,10 +2,39 @@ package com.uma.menpas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DudasSugerencias : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dudas_sugerencias)
+
+        val barraNavegacionInferior = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        barraNavegacionInferior.menu.getItem(0).isCheckable =
+            false; // LINEA PROVISIONAL PARA QUITAR EL FOCUS INICIAL DEL PRIMER ELEMENTO
+        barraNavegacionInferior.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_faq -> {
+                    it.isCheckable =
+                        true; // LINEA PROVISIONAL PARA DEVOLVER EL CHECKABLE AL PRIMER ELEMENTO
+                    Toast.makeText(applicationContext, "FAQ", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_home -> {
+                    Toast.makeText(applicationContext, "HOME", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_user -> {
+                    Toast.makeText(applicationContext, "USER", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
