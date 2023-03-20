348012b3a5ec9dc423e303070d5cce2687d98519
MiguelAngelCabanillas <0619851753@uma.es>
2023-03-20 14:36:52 +0100

    Focus de barra quitado

--- app/src/main/java/com/uma/menpas/Autores.kt
+++ app/src/main/java/com/uma/menpas/Autores.kt
@@ -2,10 +2,37 @@ package com.uma.menpas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Autores : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_autores)
+
+        val barraNavegacionInferior = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        barraNavegacionInferior.menu.getItem(0).isCheckable = false;
+        barraNavegacionInferior.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_faq -> {
+                    it.setCheckable(true); // LINEA PROVISIONAL PARA DEVOLVER EL CHECKABLE AL PRIMER ELEMENTO
+                    Toast.makeText(applicationContext, "FAQ", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_home -> {
+                    Toast.makeText(applicationContext, "HOME", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_user -> {
+                    Toast.makeText(applicationContext, "USER", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
