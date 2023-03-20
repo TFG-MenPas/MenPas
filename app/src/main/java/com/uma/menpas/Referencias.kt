02ccf6901db40a23875a3c429713652f064af853
MiguelAngelCabanillas <0619851753@uma.es>
2023-03-20 14:35:21 +0100

    Focus quitado de la barra de navegación

--- app/src/main/java/com/uma/menpas/Referencias.kt
+++ app/src/main/java/com/uma/menpas/Referencias.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Referencias : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +14,34 @@ class Referencias : AppCompatActivity() {
 
         val textLinkReferencia1 = findViewById<TextView>(R.id.textLinkReferencia1)
         val textLinkReferencia2 = findViewById<TextView>(R.id.textLinkReferencia2)
+        val barraNavegacionInferior = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
         textLinkReferencia1.movementMethod = LinkMovementMethod.getInstance();
         textLinkReferencia2.movementMethod = LinkMovementMethod.getInstance();
+
+
+
+        barraNavegacionInferior.getMenu().getItem(0).setCheckable(false);
+        barraNavegacionInferior.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_faq -> {
+                    it.setCheckable(true); // LINEA PROVISIONAL PARA DEVOLVER EL CHECKABLE AL PRIMER ELEMENTO
+                    Toast.makeText(applicationContext, "FAQ", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_home -> {
+                    Toast.makeText(applicationContext, "HOME", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_user -> {
+                    Toast.makeText(applicationContext, "USER", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
