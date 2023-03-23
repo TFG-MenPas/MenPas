2ca3cb120a56573f971b51daa8cf115c60e4dcc6
franbono2 <franbono2@gmail.com>
2023-03-23 20:08:07 +0100

    cuestionario si o no implementado en la plantilla

--- app/src/main/java/com/uma/menpas/CuestionarioGenerico.kt
+++ app/src/main/java/com/uma/menpas/CuestionarioGenerico.kt
@@ -2,10 +2,31 @@ package com.uma.menpas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.RelativeLayout
 
 class CuestionarioGenerico : AppCompatActivity() {
+    lateinit var rlDinamico: RelativeLayout
+    lateinit var btnSi: Button
+    lateinit var btnNo: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cuestionario_generico)
+
+        rlDinamico = findViewById(R.id.RLDynamicContent)
+        val cuestionarioSiNo: View = layoutInflater.inflate(R.layout.cuestionario_si_no, rlDinamico, false)
+        rlDinamico.addView(cuestionarioSiNo)
+
+        btnSi = findViewById(R.id.buttonSi)
+        btnNo = findViewById(R.id.buttonNo)
+
+        val checked = getDrawable(R.drawable.icon_checked)
+        val backChecked = getDrawable(R.drawable.rounded_checkbox_checked)
+
+        btnSi.setOnClickListener {
+            btnSi.background = backChecked
+            btnSi.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,checked,null)
+        }
     }
 }
