d30d8b73afafb3d2b960a4378106c7a9e74658bb
manuel-agm <manuel.agm@uma.es>
2023-03-25 20:38:44 +0100

    Desarrollo de FAQ y adaptadores, flujo de áreas y arreglo de márgenes de menu principal

--- app/src/main/java/com/uma/menpas/FAQ.kt
+++ app/src/main/java/com/uma/menpas/FAQ.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import android.util.TypedValue
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.SearchView
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.searchviewkotlin.PreguntaFAQAdaptador
 import com.ms.square.android.expandabletextview.ExpandableTextView
 import org.json.JSONArray
 import org.json.JSONObject
@@ -17,77 +21,54 @@ import java.io.IOException
 import java.io.InputStreamReader
 
 class FAQ : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private var mList = ArrayList<PreguntaFAQ>()
+    private lateinit var adapter: PreguntaFAQAdaptador
 
-    companion object {
-        private const val JSON_RESOURCE_NAME = "faq"
-        private const val JSON_RESOURCE_TYPE = "raw"
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_faq)
         window.decorView.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
-        val linearLayout = findViewById<LinearLayout>(R.id.expandable_layout_faq)
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView_FAQ)
 
-        val json = getJSON().toDict()
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        addDataToList()
+        adapter = PreguntaFAQAdaptador(mList)
+        recyclerView.adapter = adapter
 
-        for (key in json.keys) {
-            val cardView = CardView(this)
-            val textView = TextView(this)
-            textView.setText(key)
-            textView.setTypeface(resources.getFont(R.font.poppins_bold))
-            textView.setTextColor(resources.getColor(R.color.dark_blue))
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
+    }
 
-            textView.setPadding(0,0,300,0)
-            val params = ViewGroup.MarginLayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
+    private fun addDataToList() {
+        mList.add(
+            PreguntaFAQ(
+                resources.getString(R.string.incidencia_rejilla_q),
+                resources.getString(R.string.incidencia_rejilla_a)
             )
-            params.setMargins(0,0,0,10)
-            cardView.layoutParams = params
-
-            cardView.addView(textView)
-            linearLayout.addView(cardView)
-        }
-    }
-
-    private fun getJSON(): JSONObject {
-        val jsonString = transformJSONtoString()
-        return JSONObject(jsonString)
-    }
-
-    private fun transformJSONtoString(): String {
-        val res = resources
-        val resId = res.getIdentifier(JSON_RESOURCE_NAME, JSON_RESOURCE_TYPE, packageName)
-
-        val inputStream = res.openRawResource(resId)
-        val reader = BufferedReader(InputStreamReader(inputStream))
-
-        val sb = StringBuilder()
-        var line: String?
-
-        try {
-            while (reader.readLine().also {line = it} != null) {
-                sb.append(line)
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
-        return sb.toString()
-    }
-
-    private fun JSONObject.toDict(): Map<String, *> = keys().asSequence().associateWith {
-        when (val value = this[it])
-        {
-            is JSONArray ->
-            {
-                val map = (0 until value.length()).associate { index -> index.toString() to value[index] }
-                JSONObject(map).toDict().values.toList()
-            }
-            is JSONObject -> value.toDict()
-            JSONObject.NULL -> null
-            else            -> value
-        }
+        )
+        mList.add(
+            PreguntaFAQ(
+                resources.getString(R.string.acceso_plataforma_q),
+                resources.getString(R.string.acceso_plataforma_a)
+            )
+        )
+        mList.add(
+            PreguntaFAQ(
+                resources.getString(R.string.admin_restringido_q),
+                resources.getString(R.string.admin_restringido_a)
+            )
+        )
+        mList.add(
+            PreguntaFAQ(
+                resources.getString(R.string.validacion_usuarios_q),
+                resources.getString(R.string.validacion_usuarios_a)
+            )
+        )
+        mList.add(
+            PreguntaFAQ(
+                resources.getString(R.string.compatibilidad_macos_q),
+                resources.getString(R.string.compatibilidad_macos_a)
+            )
+        )
     }
 }
