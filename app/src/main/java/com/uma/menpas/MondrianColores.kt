f5c34e39d786d99fe065f99bccc0c7ec142b1358
franbono2 <franbono2@gmail.com>
2023-03-31 20:43:21 +0200

    grid con checkbox de los colores

---
+++ app/src/main/java/com/uma/menpas/MondrianColores.kt
@@ -0,0 +1,11 @@
+package com.uma.menpas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MondrianColores : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mondrian_colores)
+    }
+}
