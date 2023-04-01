a9b69737fd58abd7e4d828a1279dc9f75478be04
franbono2 <franbono2@gmail.com>
2023-04-01 18:42:31 +0200

    activity grid de colores

---
+++ app/src/main/java/com/uma/menpas/MondrianColoresGrid.kt
@@ -0,0 +1,11 @@
+package com.uma.menpas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MondrianColoresGrid : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mondrian_colores_grid)
+    }
+}
