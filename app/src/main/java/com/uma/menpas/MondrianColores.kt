5eab6766fa490028f452fc9b8ba8cf66928fd346
franbono2 <franbono2@gmail.com>
2023-04-02 18:08:06 +0200

    mondrian colores rellena con colores elegidos aleatoriamente

--- app/src/main/java/com/uma/menpas/MondrianColores.kt
+++ app/src/main/java/com/uma/menpas/MondrianColores.kt
@@ -1,11 +1,42 @@
 package com.uma.menpas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.GridLayout
+import android.widget.ImageButton
+import androidx.appcompat.content.res.AppCompatResources
 
 class MondrianColores : AppCompatActivity() {
+    lateinit var botonComenzar : Button
+    lateinit var colores : GridLayout
+    lateinit var checkBoxColor : CheckBox
+    lateinit var arrayColores : ArrayList<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mondrian_colores)
+
+        botonComenzar = findViewById(R.id.buttonComenzar)
+        botonComenzar.setOnClickListener {
+            val intent = Intent(this, MondrianColoresGrid::class.java)
+            colores = findViewById(R.id.gridColores)
+            arrayColores = ArrayList()
+            for (i in 0 until colores.childCount){
+                checkBoxColor = colores.getChildAt(i) as CheckBox
+                var color : String
+                if (checkBoxColor.isChecked){
+                    color = checkBoxColor.text as String
+                    arrayColores.add(color.lowercase())
+                }
+            }
+
+            intent.putExtra("arrayColores", arrayColores)
+
+            startActivity(intent)
+        }
+
+
     }
 }
--- app/src/main/java/com/uma/menpas/MondrianColoresGrid.kt
+++ app/src/main/java/com/uma/menpas/MondrianColoresGrid.kt
@@ -2,10 +2,40 @@ package com.uma.menpas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import android.widget.ImageButton
+import androidx.appcompat.content.res.AppCompatResources
 
 class MondrianColoresGrid : AppCompatActivity() {
+    lateinit var colores : GridLayout
+    lateinit var botonColor : ImageButton
+    lateinit var arrayColores : ArrayList<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mondrian_colores_grid)
+
+        arrayColores = intent.getStringArrayListExtra("arrayColores")!!
+
+        colores = findViewById(R.id.gridColores)
+        for (i in 0 until colores.childCount){
+            botonColor = colores.getChildAt(i) as ImageButton
+            val color = when(arrayColores[(0 until arrayColores.size).random()]){
+                "rojo" -> R.color.rojo
+                "marron" -> R.color.marron
+                "verde" -> R.color.verde
+                "gris" -> R.color.gris
+                "azul" -> R.color.azul
+                "rosa" -> R.color.rosa
+                "amarillo" -> R.color.amarillo
+                "violeta" -> R.color.violeta
+                "negro" -> R.color.black
+                "fucsia" -> R.color.fucsia
+                "naranja" -> R.color.naranja
+                "cyan" -> R.color.cyan
+                else -> R.color.black
+            }
+            botonColor.background = AppCompatResources.getDrawable(this, color)
+        }
+
     }
 }
