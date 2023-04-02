4b6dfade25b1b97f1278b329c16d330f7a476c4b
franbono2 <franbono2@gmail.com>
2023-04-02 19:42:30 +0200

    seleccionar color con desplegable al hacer doble click

--- app/src/main/java/com/uma/menpas/MondrianColoresGrid.kt
+++ app/src/main/java/com/uma/menpas/MondrianColoresGrid.kt
@@ -2,14 +2,18 @@ package com.uma.menpas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.GridLayout
 import android.widget.ImageButton
+import android.widget.RelativeLayout
 import androidx.appcompat.content.res.AppCompatResources
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MondrianColoresGrid : AppCompatActivity() {
     lateinit var colores : GridLayout
     lateinit var botonColor : ImageButton
     lateinit var arrayColores : ArrayList<String>
+    lateinit var botonResolver : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mondrian_colores_grid)
@@ -35,6 +39,101 @@ class MondrianColoresGrid : AppCompatActivity() {
                 else -> R.color.black
             }
             botonColor.background = AppCompatResources.getDrawable(this, color)
+            botonColor.setOnClickListener {
+                val dialog = BottomSheetDialog(this)
+                val view = layoutInflater.inflate(R.layout.desplegable_colores, null)
+                val btnColor = colores.focusedChild as ImageButton
+                btnColor.requestFocus()
+
+                val btnColorRojo = view.findViewById<ImageButton>(R.id.color00)
+                btnColorRojo.setOnClickListener {
+                    btnColor.background = btnColorRojo.background
+                    dialog.dismiss()
+                }
+
+                val btnColorVerde = view.findViewById<ImageButton>(R.id.color01)
+                btnColorVerde.setOnClickListener {
+                    btnColor.background = btnColorVerde.background
+                    dialog.dismiss()
+                }
+
+                val btnColorAzul = view.findViewById<ImageButton>(R.id.color02)
+                btnColorAzul.setOnClickListener {
+                    btnColor.background = btnColorAzul.background
+                    dialog.dismiss()
+                }
+
+                val btnColorAmarillo = view.findViewById<ImageButton>(R.id.color03)
+                btnColorAmarillo.setOnClickListener {
+                    btnColor.background = btnColorAmarillo.background
+                    dialog.dismiss()
+                }
+
+                val btnColorMarron = view.findViewById<ImageButton>(R.id.color10)
+                btnColorMarron.setOnClickListener {
+                    btnColor.background = btnColorMarron.background
+                    dialog.dismiss()
+                }
+
+                val btnColorGris = view.findViewById<ImageButton>(R.id.color11)
+                btnColorGris.setOnClickListener {
+                    btnColor.background = btnColorGris.background
+                    dialog.dismiss()
+                }
+
+                val btnColorRosa = view.findViewById<ImageButton>(R.id.color12)
+                btnColorRosa.setOnClickListener {
+                    btnColor.background = btnColorRosa.background
+                    dialog.dismiss()
+                }
+
+                val btnColorVioleta = view.findViewById<ImageButton>(R.id.color13)
+                btnColorVioleta.setOnClickListener {
+                    btnColor.background = btnColorVioleta.background
+                    dialog.dismiss()
+                }
+
+                val btnColorNegro = view.findViewById<ImageButton>(R.id.color20)
+                btnColorNegro.setOnClickListener {
+                    btnColor.background = btnColorNegro.background
+                    dialog.dismiss()
+                }
+
+                val btnColorNaranja = view.findViewById<ImageButton>(R.id.color21)
+                btnColorNaranja.setOnClickListener {
+                    btnColor.background = btnColorNaranja.background
+                    dialog.dismiss()
+                }
+
+                val btnColorCyan = view.findViewById<ImageButton>(R.id.color22)
+                btnColorCyan.setOnClickListener {
+                    btnColor.background = btnColorCyan.background
+                    dialog.dismiss()
+                }
+
+                val btnColorFucsia = view.findViewById<ImageButton>(R.id.color23)
+                btnColorFucsia.setOnClickListener {
+                    btnColor.background = btnColorFucsia.background
+                    dialog.dismiss()
+                }
+
+                val btnCerrar = view.findViewById<ImageButton>(R.id.imageButtonCerrarDesplegable)
+                btnCerrar.setOnClickListener{
+                    dialog.dismiss()
+                }
+
+                dialog.setCancelable(false)
+                dialog.setContentView(view)
+                dialog.show()
+            }
+        }
+        //Boton para it a resolver el cuestionario TODO Timer
+        botonResolver = findViewById(R.id.buttonResolver)
+        botonResolver.setOnClickListener {
+            for (i in 0 until colores.childCount){
+                botonColor = colores.getChildAt(i) as ImageButton
+                botonColor.background = AppCompatResources.getDrawable(this, R.color.elipseBlue)
+            }
         }
 
     }
