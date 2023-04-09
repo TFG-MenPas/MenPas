16ec96f3e96bd575dde3512bdf9ede16a281ba7b
magmerino <manuelantonio.gomez@dedalus.com>
2023-04-09 15:39:45 +0200

    Renombre de actividades y diseño de la pestaña de áreas, a falta de meter el contenido del JSON

--- app/src/main/java/com/uma/menpas/Areas.kt
+++ app/src/main/java/com/uma/menpas/Areas.kt
@@ -1,14 +1,12 @@
 package com.uma.menpas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.Gravity
 import android.view.ViewGroup
-import android.widget.ExpandableListView
-import android.widget.LinearLayout
-import android.widget.SearchView
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,11 +19,17 @@ import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
+import java.text.Normalizer
 
 class Areas : AppCompatActivity() {
     private lateinit var areasAdaptador: AreasAdaptador
     private lateinit var areasList: List<String>
     private lateinit var subareasList: HashMap<String,List<String>>
+
+    companion object {
+        private const val JSON_RESOURCE_NAME = "areas"
+        private const val JSON_RESOURCE_TYPE = "raw"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,38 +41,71 @@ class Areas : AppCompatActivity() {
         val areas_listView = findViewById<ExpandableListView>(R.id.areas_listView)
         areas_listView.setAdapter(areasAdaptador)
 
-        val barraNavegacionInferior = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        BarraNavegacion(barraNavegacionInferior, this)
 
-        /*
-        recyclerView = findViewById<RecyclerView>(R.id.recyclerView_FAQ)
-
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        addDataToList()
-        adapter = PreguntaFAQAdaptador(mList)
-        recyclerView.adapter = adapter
 
         val barraNavegacionInferior = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         BarraNavegacion(barraNavegacionInferior, this)
 
-         */
+        areas_listView.setOnClickListener {
+            Toast.makeText(applicationContext, "Solicitud enviada con éxito", Toast.LENGTH_SHORT)
+                .show()
+            finish()
+        }
 
     }
 
     private fun showList() {
+        val jsonObject = JSONObject(transformJSONtoString()).toDict()
         areasList = ArrayList()
         subareasList = HashMap()
 
-        (areasList as ArrayList<String>).add("Area 1")
-        (areasList as ArrayList<String>).add("Area 2")
+        var i = 0
 
-        val subarea1 : MutableList<String> = ArrayList()
-        subarea1.add("Subarea1")
-        subarea1.add("Subarea2")
+        for (key in jsonObject.keys) {
+            (areasList as ArrayList<String>).add(key)
+            var list = jsonObject[key] as List<String>
+            val subarea : MutableList<String> = ArrayList()
+            for (value in list) {
+                subarea.add(value)
+                subareasList[areasList[i]] = subarea
+            }
+            i++
+        }
+    }
 
-        subareasList[areasList[0]] = subarea1
-        subareasList[areasList[1]] = subarea1
+    private fun transformJSONtoString(): String {
+        val res = resources
+        val resId = res.getIdentifier(Areas.JSON_RESOURCE_NAME, Areas.JSON_RESOURCE_TYPE, packageName)
+
+        val inputStream = res.openRawResource(resId)
+        val reader = BufferedReader(InputStreamReader(inputStream))
+
+        val sb = StringBuilder()
+        var line: String?
+
+        try {
+            while (reader.readLine().also {line = it} != null) {
+                sb.append(line)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return sb.toString()
+    }
+
+    private fun JSONObject.toDict(): Map<String, *> = keys().asSequence().associateWith {
+        when (val value = this[it])
+        {
+            is JSONArray ->
+            {
+                val map = (0 until value.length()).associate { index -> index.toString() to value[index] }
+                JSONObject(map).toDict().values.toList()
+            }
+            is JSONObject -> value.toDict()
+            JSONObject.NULL -> null
+            else            -> value
+        }
     }
 
 }
