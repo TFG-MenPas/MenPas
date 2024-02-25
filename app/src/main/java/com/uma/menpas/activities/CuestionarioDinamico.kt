d6c59b1ec59d17b081c42644f37ff97f2b4711df
franbono2 <franbono2@gmail.com>
2024-02-25 19:44:26 +0100

    Añadido funcionalidad boton cerrar

--- app/src/main/java/com/uma/menpas/activities/CuestionarioDinamico.kt
+++ app/src/main/java/com/uma/menpas/activities/CuestionarioDinamico.kt
@@ -78,6 +78,9 @@ class CuestionarioDinamico : AppCompatActivity() {
                 rellenarPregunta(preguntas)
             }
         }
+        botonCerrarCuestionario.setOnClickListener {
+            finish()
+        }
     }
 
     private fun guardarRespuesta(tipoPregunta: String) {
