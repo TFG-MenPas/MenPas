528c59487d60fd9ab220246fdf88e3b7ce1c1d05
danijlg <danijlg92@uma.es>
2024-04-21 18:24:54 +0200

    resultados cuestionarios on hold

---
+++ app/src/main/java/com/uma/menpas/activities/ResultadosCuestionario.kt
@@ -0,0 +1,12 @@
+package com.uma.menpas.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uma.menpas.R
+
+class ResultadosCuestionario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultados_cuestionario)
+    }
+}
