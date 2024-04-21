6db067bcbde2b014145a818797b45dbd6209d03a
danijlg <danijlg92@uma.es>
2024-04-21 19:52:54 +0200

    loading dialog done

---
+++ app/src/main/java/com/uma/menpas/utils/LoadingDialog.kt
@@ -0,0 +1,40 @@
+package com.uma.menpas.utils
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.widget.ProgressBar
+import androidx.core.content.ContextCompat
+import com.uma.menpas.R
+
+class LoadingDialog(context: Context) : Dialog(context) {
+
+    private val dialog: Dialog = Dialog(context)
+
+    init {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.loading_dialog, null)
+
+        dialog.setContentView(view)
+        dialog.setCancelable(false)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val progressBar = view.findViewById<ProgressBar>(R.id.loading_spinner)
+        val color = ContextCompat.getColor(context, R.color.azul_uma)
+        progressBar.getIndeterminateDrawable()
+            .setColorFilter(color, PorterDuff.Mode.SRC_IN);
+
+    }
+
+    override fun show() {
+        dialog.show()
+    }
+
+    override fun dismiss() {
+        dialog.dismiss()
+    }
+
+}
