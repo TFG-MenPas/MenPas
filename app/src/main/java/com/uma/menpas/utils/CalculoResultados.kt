9fe8c40a4e31605ace7e9a01acf5e8aba191f9e7
unknown <0619851753@uma.es>
2024-05-04 00:41:00 +0200

    Preguntas P.Davidson divididas

--- app/src/main/java/com/uma/menpas/utils/CalculoResultados.kt
+++ app/src/main/java/com/uma/menpas/utils/CalculoResultados.kt
@@ -3,6 +3,7 @@ package com.uma.menpas.utils
 import android.content.Context
 import com.uma.menpas.cuestionarios.EPI
 import com.uma.menpas.cuestionarios.IPED
+import com.uma.menpas.cuestionarios.PDavidson
 
 class CalculoResultados {
 
@@ -15,6 +16,7 @@ class CalculoResultados {
         return when (jsonResourceName) {
             "preguntas_evaluacion_mental_iped" -> IPED.calculateIPED(respuestasUsuario, usuario)
             "preguntas_entrenamiento_mental_epi" -> EPI.calculateEPI(respuestasUsuario, context)
+            "preguntas_davidson_completo" -> PDavidson.calculatePDavidsonCompleto(respuestasUsuario, usuario)
             else -> IPED.calculateIPED(respuestasUsuario, usuario)
         }
     }
