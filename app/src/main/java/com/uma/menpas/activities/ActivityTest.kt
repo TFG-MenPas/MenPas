4a37396aa173db2882f788f763ff1428fd35ac5a
unknown <0619851753@uma.es>
2024-05-14 21:58:56 +0200

    Fix layout

---
+++ app/src/main/java/com/uma/menpas/activities/ActivityTest.kt
@@ -0,0 +1,12 @@
+package com.uma.menpas.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uma.menpas.R
+
+class ActivityTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modrian_parejas_grid)
+    }
+}
